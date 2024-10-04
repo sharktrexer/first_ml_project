4ba72cee3c18ed59b2c59e28560b01def2051cc8
sharktrexer <sharktrexer@gmail.com>
2024-10-04 08:27:00 -0700

    first commit

---
+++ main.py
@@ -0,0 +1,87 @@
+'''Tutorial from
+https://machinelearningmastery.com/machine-learning-in-python-step-by-step/'''
+
+# Ensure libraries are installed
+'''from package_test import test
+test()'''
+
+# Used libraries
+from pandas import read_csv
+from pandas.plotting import scatter_matrix
+from matplotlib import pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import StratifiedKFold
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
+
+# Load dataset
+url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/iris.csv"
+names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
+dataset = read_csv(url, names=names)
+
+'''
+Data printing
+# shape
+print(dataset.shape)
+print('\n')
+
+# head
+print(dataset.head(20))
+print('\n')
+
+# descriptions
+print(dataset.describe())
+print('\n')
+
+# class distribution
+print(dataset.groupby('class').size())
+print('\n')
+'''
+
+'''
+Visualizations
+# box and whisker plots
+dataset.plot(kind='box', subplots=True, layout=(2,2), sharex=False, sharey=False)
+plt.show()
+
+# histograms
+dataset.hist()
+plt.show()
+
+# scatter plot matrix
+scatter_matrix(dataset)
+plt.show()
+'''
+
+# Split-out validation dataset
+array = dataset.values
+X = array[:,0:4]
+y = array[:,4]
+X_train, X_validation, Y_train, Y_validation = train_test_split(X, y, test_size=0.20, random_state=1)
+
+
+# Spot Check Algorithms
+models = []
+models.append(('LR', LogisticRegression(solver='liblinear', multi_class='ovr')))
+models.append(('LDA', LinearDiscriminantAnalysis()))
+models.append(('KNN', KNeighborsClassifier()))
+models.append(('CART', DecisionTreeClassifier()))
+models.append(('NB', GaussianNB()))
+models.append(('SVM', SVC(gamma='auto')))
+# evaluate each model in turn
+results = []
+names = []
+for name, model in models:
+ kfold = StratifiedKFold(n_splits=10, random_state=1, shuffle=True)
+ cv_results = cross_val_score(model, X_train, Y_train, cv=kfold, scoring='accuracy')
+ results.append(cv_results)
+ names.append(name)
+ print('%s: %f (%f)' % (name, cv_results.mean(), cv_results.std()))
